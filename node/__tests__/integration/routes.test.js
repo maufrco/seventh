3c47068f996891a5e06b5d15e5a5cbe6f1a808f8
Mauricio de Oliveira <maufrco@gmail.com>
2020-04-06 08:48:10 -0300

    Node test

--- node/__tests__/integration/routes.test.js
+++ node/__tests__/integration/routes.test.js
@@ -1,22 +1,39 @@
 const request = require('supertest');
 const app = require('../../src/app');
-const http = require('http');
-
-
 
 describe('Api Endpoints', () => {
-    it('should fetch a single post', async ()=> {
-        
-      http.post('/host')
-        .send({
-          userId: 1,
-          name: 'test is cool',
-          protocol: 'https://',
-          domain: 'www.seventh.com.br',
-          path: '/',
-        });
-        console.log(res.statusCode)
-      expect(res.statusCode).toEqual(200);
-      expect(res.body).toHaveProperty('post');
-    })
-})
+     
+  it('should fetch all hosts',async done => {
+      const response = await request(app).get('/host')
+      expect(response.status).toBe(200)
+      done()
+    });
+
+   
+    it('should fetch all monitors', async done => {
+        const response = await request(app).get('/monitor')
+        expect(response.status).toBe(200)
+        done()
+    });
+
+
+    it('should post a host',async done => {
+
+      request(app).post('/host').send({
+        name: 'test is cool',
+        protocol: 'https://',
+        domain: 'www.seventh.com.br',
+        path: '/',
+      }).set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        done()
+    });
+   
+
+    it('should fetch all hosts',async done => {
+      const response = await request(app).delete('/host/2')
+      expect(response.status).toBe(200)
+      done()
+    });
+  });
