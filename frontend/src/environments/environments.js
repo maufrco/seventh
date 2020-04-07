ccaa6c59f00b40a0e57ab42469ad1c7348ce3793
Mauricio de Oliveira <maufrco@gmail.com>
2020-04-07 09:11:18 -0300

    fix env to server

--- frontend/src/environments/environments.js
+++ frontend/src/environments/environments.js
@@ -1,4 +1,3 @@
-export const SERVER_URL = 'http://localhost:8080/';
+//export const SERVER_URL = 'http://localhost:8080/';
 export const DEFAULT_TIMEOUT = 6000;
-
-//export const SERVER_URL = 'http://54.198.3.162:8080/';
+export const SERVER_URL = 'http://54.198.3.162:8080/';
